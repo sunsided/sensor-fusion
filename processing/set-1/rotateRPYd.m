fed9d394f5550dbb81f4ffa171e77937f15c537a
Markus Mayer <development@widemeadows.de>
2013-11-19 05:23:02 +0100

    removed orientation test files

--- processing/set-1/rotateRPYd.m
+++
@@ -1,9 +0,0 @@
-function [ rotatedVector ] = rotateRPYd( vector, rollX, pitchY, yawZ )
-%ROTATE Rotates a vector (roll-pitch-yaw order in degree)
-%   Rotates a vector using roll/pitch/yaw angles in degree
-
-    scale = pi/180;
-    rotatedVector = rotateRPY(vector, rollX*scale, pitchY*scale, yawZ*scale);
-    
-end
-
