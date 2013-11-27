19b41aa83273520825945016d15444471a59d926
Markus Mayer <development@widemeadows.de>
2013-11-27 23:23:30 +0100

    added titles to trajectory plot

--- processing/set-1/trajectory/plotTrajectory.m
+++ processing/set-1/trajectory/plotTrajectory.m
@@ -1,6 +1,6 @@
 function plotTrajectory(xyz)
 
-    global trajectoryPlotHandle
+    global trajectoryPlotHandle trajectoryAxis
     
     % Draw point
     x = [get(trajectoryPlotHandle(1), 'XData') xyz(1)];
@@ -13,6 +13,18 @@ function plotTrajectory(xyz)
     y = [0 xyz(2)];
     z = [0 xyz(3)];
     set(trajectoryPlotHandle(2), 'XData', x, 'YData', y, 'ZData', z);
+    
+    % Draw point
+    x = xyz(1);
+    y = xyz(2);
+    z = xyz(3);
+    set(trajectoryPlotHandle(3), 'XData', x, 'YData', y, 'ZData', z);
+    
+    msg = sprintf('%+1.3f %+1.3f %+1.3f', x, y, z);
+    title(trajectoryAxis, msg, 'Color', [1 1 1]);
+    xlabel(trajectoryAxis, ['x: ' num2str(x)]);
+    ylabel(trajectoryAxis, ['y: ' num2str(y)]);
+    zlabel(trajectoryAxis, ['z: ' num2str(z)]);
     
     drawnow;
 end
