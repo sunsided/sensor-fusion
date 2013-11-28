8301bfc6de507ecb890e57fdffe9685b75326135
Markus Mayer <development@widemeadows.de>
2013-11-28 22:50:20 +0100

    working on compass heading

--- processing/set-1/trajectory/simulateTrajectory.m
+++ processing/set-1/trajectory/simulateTrajectory.m
@@ -1,7 +1,8 @@
 %clear all; close all; clc; home;
 
 % define the data set folder
-dataSetFolder = fullfile(fileparts(which(mfilename)), '..', '..' , '..', 'data', 'set-1', 'rotate-360ccw-around-x-pointing-forward');
+dataSet = 'rotate-360ccw-around-z-pointing-up';
+dataSetFolder = fullfile(fileparts(which(mfilename)), '..', '..' , '..', 'data', 'set-1', dataSet);
 
 % add parent folder to path
 path(fullfile(fileparts(which(mfilename)), '..'), path);
@@ -10,7 +11,7 @@ path(fullfile(fileparts(which(mfilename)), '..'), path);
 [accelerometer, gyroscope, magnetometer, temperature] = loadData(dataSetFolder);
 
 %% Prepare Plots
-preparePlotTrajectory();
+preparePlotTrajectory(dataSet);
 
 %% Animation
 N = min(size(accelerometer,1), size(magnetometer,1));
@@ -27,16 +28,30 @@ for n=1:N
     % Calibrate values
     a = calibrateAccelerometer(a);
     m = calibrateCompass(m);
-      
+    
+    % Unfortunately, when reading the HMC5883L, the Y and Z readings
+    % are swapped, so the order is X, Z, Y.
+    % Also, for the Drotek breakout, positive Z is pointing down.
+    m = [m(1); m(3); -m(2)];
+    
     % Normalize for later use
     an = a/norm(a);
     mn = m/norm(m);
-          
+    
     % Debugging
+    %{
     msg = sprintf('acc: %+1.3f %+1.3f %+1.3f mag: %+1.3f %+1.3f %+1.3f', ... 
                     a(1), a(2), a(3), m(1), m(2), m(3));
     disp(msg);
+    %}
     
+    theta_Mx = atan2(m(2), m(1)) * 180/pi;
+    theta_My = atan2(m(3), m(2)) * 180/pi;
+    theta_Mz = atan2(m(1), m(3)) * 180/pi;
+    msg = sprintf('theta: %+1.3f %+1.3f %+1.3f', ... 
+                    theta_Mx, theta_My, theta_Mz);
+    disp(msg);
+        
     % plot the orientation
-    plotTrajectory(a);
+    plotTrajectory(m);
 end
