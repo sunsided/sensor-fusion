99a4f71825bc23b97ef47d5038e923b7623d3cce
Markus Mayer <development@widemeadows.de>
2013-12-04 04:22:08 +0100

    fixed implementation, works now

--- processing/set-2/yawPitchRoll.m
+++ processing/set-2/yawPitchRoll.m
@@ -47,31 +47,11 @@ function [azimuthYaw, elevationPitch, roll, DCM, coordinateSystem] = yawPitchRol
         ]'; % note the inverse to represent the local frame
 
     % extract angles
-    cosPitchYsinRollX = -DCM(2,3);
-    cosPitchYcosRollX =  DCM(3,3);
-    rollX = -atan2(cosPitchYsinRollX, cosPitchYcosRollX) * 180/pi;
-
-    cosPitchYcosYawZ =  DCM(1,1);
-    cosPitchYsinYawZ = -DCM(1,2);
-    yawZ = -atan2(cosPitchYsinYawZ, cosPitchYcosYawZ) * 180/pi;
-
-    sinYawZ = sind(yawZ);
-    cosYawZ = cosd(yawZ);
-    sinPitchY = DCM(1,3);
-
-    %pitchY1 = -atan2(sinPitchY, cosPitchYsinYawZ/sinYawZ) * 180/pi
-    %pitchY2 = -atan2(sinPitchY*sinYawZ, cosPitchYsinYawZ) * 180/pi
-    pitchY3 = -atan2(sinPitchY, cosPitchYcosYawZ/cosYawZ) * 180/pi;
-    %pitchY4 = -atan2(sinPitchY*cosYawZ, cosPitchYcosYawZ) * 180/pi
-    %pitchY4 = -atan2(sinPitchY, cosPitchYsinRollX/sind(rollX)) * 180/pi
-    %pitchY5 = -atan2(sinPitchY*sind(rollX), cosPitchYsinRollX) * 180/pi
-    %pitchY6 = -atan2(sinPitchY, cosPitchYcosRollX/cosd(rollX)) * 180/pi
-    pitchY7 = -atan2(sinPitchY*cosd(rollX), cosPitchYcosRollX) * 180/pi;
-    if rollX >= 90 || rollX <= -90
-        pitchY = pitchY3;
-    else
-        pitchY = pitchY7;
-    end
+    % see: William Premerlani, "Computing Euler Angles from Direction Cosines"
+    %      Indices differ from his version due to the transposed DCM.
+    pitchY = -asind(DCM(3, 1));
+    rollX = atan2d(DCM(3, 2), DCM(3, 3));
+    yawZ = atan2d(DCM(2, 1), DCM(1, 1));
 
     %% Fetch angles
 
