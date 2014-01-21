93aedc4884d182e2fd1d2b791610f114bcf72d0d
Markus Mayer <development@widemeadows.de>
2014-01-21 01:32:57 +0100

    predicting with Q

--- processing/set-2/kf_predict.m
+++ processing/set-2/kf_predict.m
@@ -4,6 +4,12 @@ function [x, P] = kf_predict(x, A, P, lambda, u, B, Q)
         lambda = 0.999;
     end
 
+    if ~exist('B', 'var') && exist('u', 'var') && ismatrix(u)
+        B = eye(size(P));
+        Q = u;
+        u = zeros(length(Q), 1);
+    end
+    
     if nargin == 3 || nargin == 4
         
         % predict state
@@ -12,7 +18,7 @@ function [x, P] = kf_predict(x, A, P, lambda, u, B, Q)
         % predict covariance
         P = A*P*A' * 1/(lambda^2);
     
-    elseif nargin == 7
+    elseif nargin == 5 || nargin == 7
     
         % predict state
         x = A*x + B*u;
